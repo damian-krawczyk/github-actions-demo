68c3803723742ca153e3ee2e4d365428e3d08d29
Damian Krawczyk <damian@krawczyk.in>
2021-03-31 22:41:06 +0200

    channel name fixed

--- test-script.py
+++ test-script.py
@@ -14,7 +14,7 @@ message_content_en = translator.translate(message_content)
 message = f"{message_content}\n{message_content_en}"
 
 try:
-    bot.send_message('@DamianTestChannell',text=message, parse_mode='Markdown')
+    bot.send_message('@DamianTestChannel',text=message, parse_mode='Markdown')
     print("Message send successfully")
 except:
     print("Message sending error")
