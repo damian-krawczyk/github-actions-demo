7f49fa08d2c722f16d5032725564d54a98d52440
Damian Krawczyk <damian@krawczyk.in>
2021-03-31 22:31:53 +0200

    telegram test

--- test-script.py
+++ test-script.py
@@ -1,6 +1,20 @@
 import os
-print("Test script run")
-secret_value = os.environ['EXAMPLE_SECRET']
+import telegram
+from translate import Translator
 
-if secret_value == 'EXAMPLE_SECRET_VALUE':
-    print('SECRET VALUE GATHERED SUCCESSFULLY')
+translator= Translator(from_lang="polish",to_lang="english")
+
+telegram_token = os.environ['TELEGRAM_TOKEN']
+
+bot = telegram.Bot(token=telegram_token)
+
+message_content = "Wiadomość testowa"
+message_content_en = translator.translate(message_content)
+
+message = f"{message_content}\n{message_content_en}"
+
+try:
+    bot.send_message('@DamianTestChannell',text=message, parse_mode='Markdown')
+    print("Message send successfully")
+except:
+    print("Message sending error")
